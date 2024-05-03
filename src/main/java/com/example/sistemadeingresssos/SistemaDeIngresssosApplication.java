85d762c35f95544969ca2021c9232888793dba70
Raissa Quezia <raissaquezia123@gmail.com>
2024-05-03 15:46:55 -0300

    feat: initial configs

---
+++ src/main/java/com/example/sistemadeingresssos/SistemaDeIngresssosApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemadeingresssos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeIngresssosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeIngresssosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistemadeingresssos/SistemaDeIngresssosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemadeingresssos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeIngresssosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
