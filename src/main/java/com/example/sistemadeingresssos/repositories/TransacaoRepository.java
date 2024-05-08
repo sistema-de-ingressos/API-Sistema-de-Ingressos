87057567c15f08eb5a7f0595dddb8d6e92fd423c
Raissa Quezia <raissaquezia123@gmail.com>
2024-05-08 14:41:09 -0300

    refactor: remoção de transação desnecessária

--- src/main/java/com/example/sistemadeingresssos/repositories/TransacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sistemadeingresssos.repositories;
-
-import com.example.sistemadeingresssos.entities.Transacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
-}
