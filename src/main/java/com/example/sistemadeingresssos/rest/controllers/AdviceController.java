6bb3e3800e472f2dcd1f8c67147ebaaf2e202ba8
Raissa Quezia <raissaquezia123@gmail.com>
2024-05-13 01:16:12 -0300

    feat: auxiliadores de excecoes e erros

---
+++ src/main/java/com/example/sistemadeingresssos/rest/controllers/AdviceController.java
@@ -0,0 +1,39 @@
+package com.example.sistemadeingresssos.rest.controllers;
+
+import com.example.sistemadeingresssos.exceptions.EventoException;
+import com.example.sistemadeingresssos.exceptions.IngressoNaoEncontradoException;
+import com.example.sistemadeingresssos.rest.ApiErros;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class AdviceController {
+    @ExceptionHandler(EventoException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErros handleRegraNegocioException(EventoException ex){
+        String mensagemErro = ex.getMessage();
+        return new ApiErros(mensagemErro);
+    }
+
+    @ExceptionHandler(IngressoNaoEncontradoException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiErros handlePedidoNotFoundException( IngressoNaoEncontradoException ex ){
+        return new ApiErros(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErros handleMethodNotValidException( MethodArgumentNotValidException ex ){
+        List<String> errors = ex.getBindingResult().getAllErrors()
+                .stream()
+                .map(erro -> erro.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ApiErros(errors);
+    }
+}
