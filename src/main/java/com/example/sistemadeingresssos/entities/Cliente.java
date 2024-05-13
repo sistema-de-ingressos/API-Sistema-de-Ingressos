41ef53a2130362c08e26625d10b944aa82d62993
Raissa Quezia <raissaquezia123@gmail.com>
2024-05-13 01:11:14 -0300

    feat: implementado bean validation

--- src/main/java/com/example/sistemadeingresssos/entities/Cliente.java
+++ src/main/java/com/example/sistemadeingresssos/entities/Cliente.java
@@ -2,12 +2,17 @@ package com.example.sistemadeingresssos.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
+import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
 
 @Entity
@@ -17,10 +22,16 @@ import java.util.Date;
 public class Cliente {
 
     @Id
+    @NotEmpty(message = "Campo obrigatório!")
+    @CPF(message = "Informe um CPF válido.")
     private String cpf;
+    @NotEmpty(message = "Campo obrigatório!")
     private String nome;
-    private Date dataDeNascimento;
+    @NotEmpty(message = "Campo obrigatório")
+    private LocalDate dataDeNascimento;
     @OneToOne
     private Endereco enderecoId;
+    @OneToMany
+    private List<Ingresso> lista;
 
 }
--- src/main/java/com/example/sistemadeingresssos/entities/Endereco.java
+++ src/main/java/com/example/sistemadeingresssos/entities/Endereco.java
@@ -2,6 +2,7 @@ package com.example.sistemadeingresssos.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,10 +16,15 @@ public class Endereco {
     @Id
     private Integer id;
     private String logradouro;
+//    @NotEmpty(message = "Campo obrigatório!")
     private String cep;
+//    @NotEmpty(message = "Campo obrigatório!")
     private String bairro;
+//    @NotEmpty(message = "Campo obrigatório!")
     private String cidade;
+//    @NotEmpty(message = "Campo obrigatório!")
     private String estado;
+//    @NotEmpty(message = "Campo obrigatório")
     private Integer numero;
     private String complemento;
 
