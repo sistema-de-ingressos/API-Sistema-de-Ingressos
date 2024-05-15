b72ca542f9c0f92cf8875bec1356bc7a8e433db6
Raissa Quezia <raissaquezia123@gmail.com>
2024-05-15 20:27:43 -0300

    feat: adicionadas funcionalidades de endereco

--- src/main/java/com/example/sistemadeingresssos/entities/Endereco.java
+++ src/main/java/com/example/sistemadeingresssos/entities/Endereco.java
@@ -1,6 +1,9 @@
 package com.example.sistemadeingresssos.entities;
 
+import com.example.sistemadeingresssos.rest.dtos.EnderecoDTO;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -14,18 +17,28 @@ import lombok.NoArgsConstructor;
 public class Endereco {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String logradouro;
-//    @NotEmpty(message = "Campo obrigatório!")
+    @NotEmpty(message = "Campo obrigatório!")
     private String cep;
-//    @NotEmpty(message = "Campo obrigatório!")
+    @NotEmpty(message = "Campo obrigatório!")
     private String bairro;
-//    @NotEmpty(message = "Campo obrigatório!")
+    @NotEmpty(message = "Campo obrigatório!")
     private String cidade;
-//    @NotEmpty(message = "Campo obrigatório!")
+    @NotEmpty(message = "Campo obrigatório!")
     private String estado;
-//    @NotEmpty(message = "Campo obrigatório")
+    @NotEmpty(message = "Campo obrigatório")
     private Integer numero;
     private String complemento;
 
+    public Endereco(EnderecoDTO enderecoDTO) {
+        this.logradouro = enderecoDTO.logradouro();
+        this.cep = enderecoDTO.cep();
+        this.bairro = enderecoDTO.bairro();
+        this.cidade = enderecoDTO.cidade();
+        this.estado = enderecoDTO.estado();
+        this.numero = enderecoDTO.numero();
+        this.complemento = enderecoDTO.complemento();
+    }
 }
--- src/main/java/com/example/sistemadeingresssos/rest/controllers/EnderecoController.java
+++ src/main/java/com/example/sistemadeingresssos/rest/controllers/EnderecoController.java
@@ -1,4 +1,4 @@
-package com.example.sistemadeingresssos.controllers;
+package com.example.sistemadeingresssos.rest.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
