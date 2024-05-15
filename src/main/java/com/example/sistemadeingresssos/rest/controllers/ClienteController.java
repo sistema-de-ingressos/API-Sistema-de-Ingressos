c3f2ba656ac127aa525a5973c190fda6b5cdf4da
Raissa Quezia <raissaquezia123@gmail.com>
2024-05-15 20:27:20 -0300

    feat: adicionadas funcionalidades de cliente

---
+++ src/main/java/com/example/sistemadeingresssos/rest/controllers/ClienteController.java
@@ -0,0 +1,58 @@
+package com.example.sistemadeingresssos.rest.controllers;
+
+import com.example.sistemadeingresssos.entities.Cliente;
+import com.example.sistemadeingresssos.repositories.ClienteRepository;
+import com.example.sistemadeingresssos.repositories.EnderecoRepository;
+import com.example.sistemadeingresssos.rest.dtos.SalvarClienteDTO;
+import com.example.sistemadeingresssos.services.ClienteService;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteController {
+
+    private ClienteRepository repository;
+    private EnderecoRepository enderecoRepository;
+    private ClienteService service;
+    private final ModelMapper modelMapper;
+
+    public ClienteController(ClienteRepository repository, ClienteService service, ModelMapper modelMapper) {
+        this.service = service;
+        this.repository = repository;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostMapping
+    public ResponseEntity salvarCliente(@RequestBody @Valid SalvarClienteDTO salvarClienteDTO) {
+
+        SalvarClienteDTO salvarClienteDTO1 = service.save(salvarClienteDTO);
+
+        return ResponseEntity.ok().body(salvarClienteDTO1);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> listarClientes(){
+        List<Cliente> clientes = service.findAll();
+        return ResponseEntity.ok().body(clientes);
+    }
+
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity deletarCliente(@PathVariable Integer id){
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @GetMapping(value = "/{cpf}")
+    public ResponseEntity buscarClienteByCpf(@PathVariable String cpf){
+        SalvarClienteDTO clienteBuscado = service.findByCpf(cpf);
+        return ResponseEntity.ok().body(clienteBuscado);
+    }
+}
--- src/main/java/com/example/sistemadeingresssos/services/ClienteService.java
+++ src/main/java/com/example/sistemadeingresssos/services/ClienteService.java
@@ -1,7 +1,77 @@
 package com.example.sistemadeingresssos.services;
 
+import com.example.sistemadeingresssos.entities.Cliente;
+import com.example.sistemadeingresssos.entities.Ingresso;
+import com.example.sistemadeingresssos.repositories.ClienteRepository;
+import com.example.sistemadeingresssos.repositories.EnderecoRepository;
+import com.example.sistemadeingresssos.rest.dtos.SalvarClienteDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteService {
+
+    private final EnderecoRepository enderecoRepository;
+    private ClienteRepository repository;
+    private final ModelMapper modelMapper;
+
+    public ClienteService(ClienteRepository repository, EnderecoRepository enderecoRepository, ModelMapper modelMapper) {
+        this.repository = repository;
+        this.enderecoRepository = enderecoRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public SalvarClienteDTO save(SalvarClienteDTO salvarClienteDTO) {
+        Cliente cliente = new Cliente(salvarClienteDTO);
+
+        Cliente clienteSalvo = repository.save(cliente);
+
+        SalvarClienteDTO clienteSalvoDTO = new SalvarClienteDTO(clienteSalvo);
+
+        return clienteSalvoDTO;
+    }
+
+    public Cliente updateListaIngresso(Integer clienteId, Cliente clienteAtualizado) {
+        Cliente clienteExistente = findByIdIngresso(clienteId);
+
+        clienteExistente.setIngressos(clienteAtualizado.getIngressos());
+
+        Cliente clienteAtualizado1 = repository.save(clienteExistente);
+
+        return clienteAtualizado1;
+    }
+
+    public List<Cliente> findAll(){
+        return repository.findAll();
+    }
+
+    public SalvarClienteDTO findById(Integer id){
+        Optional<Cliente> clienteOptional = repository.findById(id);
+        return clienteOptional.map(SalvarClienteDTO::new)
+                .orElse(null);
+    }
+
+    public SalvarClienteDTO findByCpf(String cpf){
+        Optional<Cliente> clienteOptional = repository.findByCpf(cpf);
+        return clienteOptional.map(cliente -> new SalvarClienteDTO(cliente))
+                .orElse(null);
+    }
+
+    public Cliente findClienteByCpf(String cpf){
+        Optional<Cliente> clienteOptional = repository.findByCpf(cpf);
+        return clienteOptional.get();
+    }
+
+    public Cliente findByIdIngresso(Integer id){
+        Optional<Cliente> cliente = repository.findById(id);
+        return cliente.orElse(null);
+    }
+
+    public void addIngresso(Ingresso ingresso, Cliente cliente){
+        cliente.getIngressos().add(ingresso);
+    }
 }
+
