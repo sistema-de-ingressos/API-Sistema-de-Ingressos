c899fec27827c908ef5914809983e5421b7ed618
Raissa Quezia <raissaquezia123@gmail.com>
2024-05-17 18:37:55 -0300

    fix: exclusão de arquivos desnecessários

--- src/main/java/com/example/sistemadeingresssos/rest/controllers/EnderecoController.java
+++
@@ -1,9 +0,0 @@
-package com.example.sistemadeingresssos.rest.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = "/endereco")
-public class EnderecoController {
-}
