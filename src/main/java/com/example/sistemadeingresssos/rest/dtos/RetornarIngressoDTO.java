494ab5ce17b803f03c4d0dc662234f04802b3359
Raíssa Passos <raissaquezia123@gmail.com>
2024-05-28 16:46:34 -0300

    fix: corrigido erro para retornar nome do cliente na busca de ingresso

--- src/main/java/com/example/sistemadeingresssos/rest/dtos/RetornarIngressoDTO.java
+++ src/main/java/com/example/sistemadeingresssos/rest/dtos/RetornarIngressoDTO.java
@@ -1,14 +1,14 @@
 package com.example.sistemadeingresssos.rest.dtos;
 
-import com.example.sistemadeingresssos.entities.Evento;
 import com.example.sistemadeingresssos.entities.Ingresso;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
-public record RetornarIngressoDTO(String nomeDoCliente, String nomeDoEvento, LocalDate dataDoEvento, Double preco, String url) {
+public record RetornarIngressoDTO(Integer idDoEvento, String nomeDoEvento, @JsonFormat(pattern = "dd/MM/yyyy") LocalDate dataDoEvento, Double total, String url) {
 
     public RetornarIngressoDTO(Ingresso ingresso) {
-        this(ingresso.getCliente().getNome(), ingresso.getEvento().getNome(), ingresso.getEvento().getData(), ingresso.getTotal(), ingresso.getUrl());
+        this(ingresso.getEvento().getId(), ingresso.getEvento().getNome(), ingresso.getEvento().getData(), ingresso.getTotal(), ingresso.getUrl());
     }
+
 }
