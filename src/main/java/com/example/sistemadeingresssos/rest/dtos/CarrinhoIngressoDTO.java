0f1d12f35abb0dd9c323af0880681cfd998c0c69
Raíssa Passos <raissaquezia123@gmail.com>
2024-05-28 16:27:08 -0300

    fix: atributo do construtor do CarrinhoIngressoDTO para idDoEvento

--- src/main/java/com/example/sistemadeingresssos/rest/dtos/CarrinhoIngressoDTO.java
+++ src/main/java/com/example/sistemadeingresssos/rest/dtos/CarrinhoIngressoDTO.java
@@ -1,6 +1,6 @@
 package com.example.sistemadeingresssos.rest.dtos;
 
-import com.example.sistemadeingresssos.entities.Ingresso;
+import com.example.sistemadeingresssos.entities.Evento;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDate;
@@ -10,10 +10,10 @@ public record CarrinhoIngressoDTO(
         String nomeDoEvento, Double valor, Double taxa, Double total, String descricao,
         String local, @JsonFormat(pattern = "dd/MM/yyyy") LocalDate data, LocalTime horario, byte[] imagemDestaque) {
 
-    public CarrinhoIngressoDTO(Ingresso ingresso) {
-        this(ingresso.getEvento().getNome(), ingresso.getEvento().getValorAtual(),
-                ingresso.getEvento().getTaxa(), ingresso.getTotal(), ingresso.getEvento().getDescricao(), ingresso.getEvento().getLocal(),
-                ingresso.getEvento().getData(), ingresso.getEvento().getHorario(), (ingresso.getEvento().getImagemPrincipal().getBase64()));
+    public CarrinhoIngressoDTO(Evento evento) {
+        this(evento.getNome(), evento.getValorAtual(),
+                evento.getTaxa(), (evento.getValorAtual() + evento.getTaxa()), evento.getDescricao(), evento.getLocal(),
+                evento.getData(), evento.getHorario(), (evento.getImagemPrincipal() == null) ? null : evento.getImagemPrincipal().getBase64());
     }
 
 }
