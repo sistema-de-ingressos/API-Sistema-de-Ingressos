580725112f0ce7df9a7b9dfecbd58176a3e3c233
Raíssa Passos <raissaquezia123@gmail.com>
2024-05-28 23:55:08 -0300

    fix: descomentado commandLineRunner

--- src/main/java/com/example/sistemadeingresssos/initializers/DataLoader.java
+++ src/main/java/com/example/sistemadeingresssos/initializers/DataLoader.java
@@ -1,10 +1,16 @@
 package com.example.sistemadeingresssos.initializers;
 
+import com.example.sistemadeingresssos.entities.Evento;
+import com.example.sistemadeingresssos.enums.EventoStatus;
+import com.example.sistemadeingresssos.rest.dtos.SalvarEventoDTO;
 import com.example.sistemadeingresssos.services.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Component
 @Profile({"teste"})
@@ -13,14 +19,14 @@ public class DataLoader implements CommandLineRunner {
     EventoService eventoService;
     @Override
     public void run(String... args) throws Exception {
-//        for (int i = 1; i <= 15; i++) {
-//            Evento evento;
-//            if (i <= 8) {
-//                evento = new Evento("Evento " + i, "Descrição do evento " + i, LocalDate.now(), LocalTime.now(), "Local do evento " + i, 100, 50.0, 200, 150, EventoStatus.QUASE_LOTADO);
-//            } else {
-//                evento = new Evento("Evento " + i, "Descrição do evento " + i, LocalDate.now(), LocalTime.now(), "Local do evento " + i, 100, 50.0, 200, 150, EventoStatus.DISPONIVEL);
-//            }
-//            eventoService.save(evento);
-//        }
+        for (int i = 1; i <= 15; i++) {
+            Evento evento;
+            if (i <= 8) {
+                evento = new Evento("Evento " + i, "Descrição do evento " + i, LocalDate.now(), LocalTime.now(), "Local do evento " + i, 100, 50.0, 200, 150, EventoStatus.QUASE_LOTADO);
+            } else {
+                evento = new Evento("Evento " + i, "Descrição do evento " + i, LocalDate.now(), LocalTime.now(), "Local do evento " + i, 100, 50.0, 200, 150, EventoStatus.DISPONIVEL);
+            }
+            eventoService.save(new SalvarEventoDTO(evento));
+        }
     }
 }
